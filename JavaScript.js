aac194c32d1adc75b9ae7f2ec37cb79d12b832ac
isabcarvalho <42363207+isabcarvalho@users.noreply.github.com>
2022-02-17 22:18:04 -0300

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+
+    const botao = document.getElementById("btnClique");
+
+    botao.addEventListener("click", loginSucesso);
+
+    function loginSucesso(){
+        alert("Login feito com sucesso!");
+        return;
+    }
